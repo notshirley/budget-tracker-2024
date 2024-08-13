bad3b4b3e65d0afe63766d7bb4cffdd49e037a9a
Shirley Wu <shirleywuz67@gmail.com>
2024-08-13 00:31:35 -0400

    Update main.js
    toggle light/dark mode, use templates, edit add button actions

--- main.js
+++ main.js
@@ -1,120 +1,59 @@
-// TODO:  Toggle light/dark mode, bootstrap!
-// TODO:  Look into HTML Templates & web components
-// TODO:  Inline table edits - no alerts. List & edit view maybe.
-
-const currentDate = new Date().toLocaleDateString()
+const currentDate = new Date().toLocaleDateString();
+const emptyRow = {name: "", price: "", type: "", dateAdded: currentDate};
 
 const expenses = [
     {
-        expense: "Groceries",
+        name: "Groceries",
         price: 50,
         type: "food",
         dateAdded: currentDate
     },
     {
-        expense: "Gas",
+        name: "Gas",
         price: 30,
         type: "bill",
         dateAdded:  currentDate
     },
     {
-        expense: "Dinner",
+        name: "Dinner",
         price: 60,
         type: "food",
         dateAdded: currentDate
     }
 ];
 
+const  table = document.querySelector('#spendingsTable');
+const headerRowtemplate = document.getElementById('headerRowtemplate').content;
+const contentRowtemplate = document.getElementById('expenseRowTemplate').content;
 
 function updateTable() {
-    const  table = document.querySelector('#spendingsTable');
     table.innerHTML = '';
 
-    const headerRow = document.createElement('tr');
-    const actionsHeader = document.createElement('th');
-    actionsHeader.textContent = 'Actions';
-    headerRow.appendChild(actionsHeader);
-
-    const expenseHeader = document.createElement('th');
-    expenseHeader.textContent = 'Expense';
-    headerRow.appendChild(expenseHeader);
-
-    const priceHeader = document.createElement('th');
-    priceHeader.textContent = 'Price';
-    headerRow.appendChild(priceHeader);
-
-    const typeHeader = document.createElement('th');
-    typeHeader.textContent = 'Type';
-    headerRow.appendChild(typeHeader);
-
-    const dateAddedHeader = document.createElement('th');
-    dateAddedHeader.textContent = 'Date Added';
-    headerRow.appendChild(dateAddedHeader);
-
+    const headerRow = document.importNode(headerRowtemplate, true);
     table.appendChild(headerRow);
     
-    expenses.forEach((spending, index) => {
-        const row = document.createElement('tr');
-    
-        const buttonsCell = document.createElement('td');
-        const deleteButton = document.createElement('button');
-        deleteButton.innerHTML = '<i class="material-icons table-button">delete</i>';
-        deleteButton.onclick = function() {
-            removeExpense(index);
-        };
+    expenses.forEach((expense, index) => {
+        const row = document.importNode(contentRowtemplate, true);
 
-        const editButton = document.createElement('button');
-        editButton.innerHTML = '<i class="material-icons table-button">edit</i>';
-        editButton.onclick = function() {
-            editExpense(index);
-        };
-    
-        buttonsCell.appendChild(deleteButton);
-        buttonsCell.appendChild(editButton);
-    
-        row.appendChild(buttonsCell);
-        
-        const expenseCell = document.createElement('td');
-        expenseCell.textContent = spending.expense;
-        row.appendChild(expenseCell);
-    
-        const priceCell = document.createElement('td');
-        priceCell.textContent = spending.price;
-        row.appendChild(priceCell);
-    
-        const typeCell = document.createElement('td');
-        typeCell.textContent = spending.type;
-        row.appendChild(typeCell);
-    
-        const dateAddedCell = document.createElement('td');
-        dateAddedCell.textContent = spending.dateAdded;
-        row.appendChild(dateAddedCell);
-        
+        row.querySelector('.delete-button').addEventListener('click', () => removeExpense(index));
+        row.querySelector('.edit-button').addEventListener('click', () => editExpense(index));
+
+        row.querySelector('.name').textContent = expense.name;
+        row.querySelector('.price').textContent = expense.price;
+        row.querySelector('.type').textContent = expense.type;
+        row.querySelector('.date-added').textContent = expense.dateAdded;
+
         table.appendChild(row);
     });
-
-    const addRow = document.createElement('tr');
-    const addButton = document.createElement('button');
-    addButton.innerHTML = '<i class="material-icons">add</i>'
-    addButton.addEventListener('click', addNewExpense);
-    addRow.appendChild(addButton);
-    table.appendChild(addRow);
 }
 
-function addNewExpense() {
-    const newExpense = prompt("What is the expense?");
-    const newPrice = prompt("How much was it?");
-    const newType = prompt("What kind of expense is it?");
+document.getElementById("add-button").addEventListener('click', () => addNewExpense());
 
-    if (newExpense !== null && newPrice !== null && newType !== null) {
-        expenses.push({
-            expense: newExpense, 
-            price: newPrice, 
-            type: newType, 
-            dateAdded: currentDate
-        })
-        updateTable()
-    }
+function addNewExpense() {
+    expenses.push({...emptyRow}); 
+    updateTable();
+    editExpense(expenses.length - 1);
+
 }
 
 function removeExpense(index) {
@@ -123,16 +62,49 @@ function removeExpense(index) {
 }
 
 function editExpense(index) {
-    const newExpense = prompt("Edit the expense:", expenses[index].expense);
-    const newPrice = prompt("Edit the price:", expenses[index].price);
-    const newType = prompt("Edit the type:", expenses[index].type);
+    const row = table.rows[index+1];
 
-    expenses[index].expense = newExpense;
-    expenses[index].price = newPrice;
-    expenses[index].type = newType;
-    expenses[index].dateAdded = currentDate;
+    row.querySelector('.name').contentEditable = true;
+    row.querySelector('.price').contentEditable = true;
+    row.querySelector('.type').contentEditable = true;
 
-    updateTable();
+    const editButton = row.querySelector('.edit-button');
+    editButton.innerHTML = '<i class="material-icons save-icon">save</i>';
+    editButton.addEventListener('click', () => saveExpense(index, row));
 }
 
+function saveExpense(index, row) {
+    expenses[index].name = row.querySelector('.name').textContent;
+    expenses[index].price = row.querySelector('.price').textContent;
+    expenses[index].type = row.querySelector('.type').textContent;
+    expenses[index].dateAdded = currentDate;
+
+    if (expenses[index].name && expenses[index].price && expenses[index].type) {
+        row.querySelector('.name').contentEditable = false;
+        row.querySelector('.price').contentEditable = false;
+        row.querySelector('.type').contentEditable = false;
+    
+        const saveButton = row.querySelector('.edit-button');
+        saveButton.innerHTML = '<i class="material-icons">edit</i>';
+        saveButton.addEventListener('click', () => editExpense(index));
+    }
+}
+
+
 updateTable()
+
+function toggleTheme() {
+    document.body.classList.toggle('bg-dark');
+    document.body.classList.toggle('text-light');
+    document.querySelectorAll('.btn').forEach(button => {
+        button.classList.toggle('text-light');
+    })
+
+    document.querySelectorAll('.material-icons').forEach(icon => {
+        icon.classList.toggle('light');
+    })
+}
+
+// Attach event listener to the theme toggle button
+document.getElementById('themeToggle').addEventListener('click', toggleTheme);
+
